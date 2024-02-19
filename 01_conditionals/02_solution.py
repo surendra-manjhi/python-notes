4e141e11acbeb8b1130f7004470646b97ad18599
Surendra <manjhisurendra08@gmail.com>
2024-02-19 23:24:23 +0530

    Solution 02

---
+++ 01_conditionals/02_solution.py
@@ -0,0 +1,18 @@
+age = int(input("Enter your age: "))
+day = "Wednesday"
+
+# 1st way
+
+price = 0
+
+if age >= 18:
+	price = 20
+else:
+	price = 8
+
+# price = 20 if age >= 18 else 8 # 2nd way
+
+if day == "Wednesday":
+	price -= 2
+
+print("Ticket price is $" + str(price))
