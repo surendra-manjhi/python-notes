30fea6df79068cd50bd5d9b8f69872afbf3a5833
Surendra <manjhisurendra08@gmail.com>
2024-02-20 00:01:08 +0530

    Added sol 06

---
+++ 01_conditionals/05_solution.py
@@ -0,0 +1,12 @@
+### Suggest activity based on Weather: Sunny (Go for a walk), Rainy (Read a book), Snowy (Build a snowman)
+
+weather = input("Enter weather: ")
+
+if weather == "Sunny":
+	print("Go for a walk")
+elif weather == "Rainy":
+	print("Read a book")
+elif weather == "Snowy":
+	print("Build a snowman")
+else:
+	print("No data found")
